65000e451e01ab2b79546a25d5daae7fb5ecb495
wenlo <wenlongzhou@aliyun.com>
2019-06-24 11:04:21 +0800

    我的订单页面修改

--- src/router/index1.js
+++ src/router/index1.js
@@ -14,6 +14,11 @@ import SubmitOrder from '@/show/SubmitOrder'
 import UserCenter from '@/show/UserCenter'
 import SellerCenter from '@/show/SellerCenter'
 import ShopManage from '@/show/ShopManage'
+import Page1 from '@/show/Page1'
+import Page2 from '@/show/Page2'
+import Page3 from '@/show/Page3'
+import Page4 from '@/show/Page4'
+import Page5 from '@/show/Page5'
 
 Vue.use(Router)
 
@@ -61,6 +66,38 @@ export default new Router({
       path: '/MyOrder',
       name: 'MyOrder',
       component: MyOrder,
+      children: [
+        {
+          path: '/',
+          name: 'Page1',
+          component: Page1
+        },
+        {
+          path: '/Page1',
+          name: 'Page1',
+          component: Page1
+        },
+        {
+          path: '/Page2',
+          name: 'Page2',
+          component: Page2
+        },
+        {
+          path: '/Page3',
+          name: 'Page3',
+          component: Page3
+        },
+        {
+          path: '/Page4',
+          name: 'Page4',
+          component: Page4
+        },
+        {
+          path: '/Page5',
+          name: 'Page5',
+          component: Page5
+        }
+      ],
       meta: {
         requireLogin: true // 当前路由需要校验，不需要就不用写
       }
